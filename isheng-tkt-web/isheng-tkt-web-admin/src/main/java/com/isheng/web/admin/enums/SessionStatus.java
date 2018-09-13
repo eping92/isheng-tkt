901ef31843441bc1dad020de060c07472bd7319b
eping92 <2228086822@qq.com>
2018-09-13 22:48:39 +0800

    更新

---
+++ isheng-tkt-web/isheng-tkt-web-admin/src/main/java/com/isheng/web/admin/enums/SessionStatus.java
@@ -0,0 +1,14 @@
+package com.isheng.web.admin.enums;
+
+public enum SessionStatus {
+	
+	/**
+	 * 没有权限
+	 */
+	noPermis,
+	/**
+	 * 网络超时
+	 */
+	timeOut;
+
+}
---
+++ isheng-tkt-web/isheng-tkt-web-admin/src/main/java/com/isheng/web/admin/security/UserRealm.java
@@ -0,0 +1,102 @@
+package com.isheng.web.admin.security;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.SimplePrincipalCollection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.isheng.common.constant.SysConfig;
+import com.isheng.model.auth.entity.Menu;
+import com.isheng.model.auth.entity.User;
+import com.isheng.service.auth.MenuService;
+import com.isheng.web.admin.common.SessionHandler;
+import com.isheng.web.admin.domain.UserSession;
+import com.isheng.web.admin.domain.UserToken;
+
+public class UserRealm extends AuthorizingRealm {
+	
+	private static final Logger logger = LoggerFactory.getLogger(UserRealm.class);
+	
+	@Reference
+	private MenuService menuService;
+	
+	/**
+	 * 认证回调函数,登录时调用.
+	 * @see org.apache.shiro.realm.AuthenticatingRealm#doGetAuthenticationInfo(org.apache.shiro.authc.AuthenticationToken)
+	 */
+	@Override
+	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+		UserToken userToken = (UserToken) token;
+		User user = userToken.getUser();
+		logger.info("账户登录，账户:{},被踢出", user.getLoginName());
+		//////TODO 从缓存中删除user session
+		UserSession sessionUser = new UserSession();
+		sessionUser.setLoginName(user.getLoginName());
+		sessionUser.setMobile(user.getMobile());
+		sessionUser.setNick(user.getNick());
+		sessionUser.setRealName(user.getRealName());
+		sessionUser.setUserId(user.getId());
+		
+		//重新放入会话中
+		SessionHandler.setSessionAttr(SysConfig.SESSION_USER_KEY, sessionUser);
+		//放入缓存中
+		logger.info("账户登录,账户:{}, sessionId:{}", user.getLoginName(), SecurityUtils.getSubject().getSession().getId());
+		
+		return new SimpleAuthenticationInfo(user.getId(), userToken.getPassword(), getName());
+	}
+
+	/**
+	 * 授权查询回调函数, 进行鉴权但缓存中无用户的授权信息时调用.
+	 * @see org.apache.shiro.realm.AuthorizingRealm#doGetAuthorizationInfo(org.apache.shiro.subject.PrincipalCollection)
+	 */
+	@Override
+	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+		String userId = (String) principals.fromRealm(getName()).iterator().next();
+		if (StringUtils.isEmpty(userId)) {
+			return null;
+		}
+		
+		SimpleAuthorizationInfo info = (SimpleAuthorizationInfo) SessionHandler.getSessionAttr(SysConfig.PERMISSIONS);
+		if (null != info) {
+			return info;
+		}
+		
+		info = new SimpleAuthorizationInfo();
+		List<Menu> menuList = new ArrayList<>();//TODO menuService.listByUserId(userId);
+		if (null != menuList && !menuList.isEmpty()) {
+			for (Menu m : menuList) {
+				info.addStringPermission(m.getCode());
+			}
+			SessionHandler.setSessionAttr(SysConfig.SESSION_USER_KEY, info);
+		}
+		
+		return info;
+	}
+
+	
+	/**
+	 * 更新用户授权信息缓存.
+     */
+    public void clearCachedAuthorizationInfo(String principal) {
+        SimplePrincipalCollection principals = new SimplePrincipalCollection(principal, getName());
+        clearCachedAuthorizationInfo(principals);
+    }
+
+    @Override
+    public Class<?> getAuthenticationTokenClass() {
+        return UserToken.class;
+    }
+
+}
