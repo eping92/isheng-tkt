6edafe239683c57d42a79d83ec3e7c1ffbc807d8
eping92 <2228086822@qq.com>
2018-09-25 21:16:18 +0800

    11

--- isheng-tkt-web/isheng-tkt-cms/src/main/resources/static/js/baseCss.js
+++
@@ -1,6 +0,0 @@
-/**
- * 公用css库
- */
-
-/** layer.css */
-document.write('<link href="/static/js/layui/css/layui.css" rel="stylesheet" type="text/css">');
--- isheng-tkt-web/isheng-tkt-cms/src/main/resources/static/js/baseJs.js
+++
@@ -1,6 +0,0 @@
-/**
- * 公共JS
- */
-
-/** layer.js */
-document.write('<script src="/static/js/layui/layui.js"  type="text/javascript" async></script>');
