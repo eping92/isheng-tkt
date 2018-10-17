f7aad9a324201dd4456a55b1fa14624bb56dd3a6
eping92 <2228086822@qq.com>
2018-10-17 14:23:12 +0800

    11111

--- isheng-tkt-common/src/main/java/com/isheng/common/base/BaseTest.java
+++ isheng-tkt-common/src/main/java/com/isheng/common/base/BaseTest.java
@@ -2,12 +2,10 @@ package com.isheng.common.base;
 
 import junit.framework.TestCase;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class BaseTest extends TestCase{
 
 }
